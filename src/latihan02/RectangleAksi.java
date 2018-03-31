f6a2003d6876346492af7b9f34e16b3be9971bc8
Aidanurp <14maret1998@gmail.com>
2018-03-31 22:19:14 +0800

    mengisi nilai atibut

--- src/latihan02/RectangleAksi.java
+++ src/latihan02/RectangleAksi.java
@@ -12,6 +12,10 @@ package latihan02;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
-       
+        r1.panjang = 10;
+        r1.lebar = 5;
+        r1.tinggi = 2;
+        
+        
     }
 }
